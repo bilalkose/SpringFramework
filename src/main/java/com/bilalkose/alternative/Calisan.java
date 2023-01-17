f04b126e5e912ddf8ee3d24a64e74f322e794da2
Bilal <bilalmuzafferkose@gmail.com>
2023-01-17 22:44:43 +0300

    interface is null bug fix

--- src/main/java/com/bilalkose/alternative/Calisan.java
+++ src/main/java/com/bilalkose/alternative/Calisan.java
@@ -1,5 +1,8 @@
 package com.bilalkose.alternative;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
@@ -8,10 +11,8 @@ import javax.inject.Named;
 @Named(value = "alternativedi")
 @ApplicationScoped
 public class Calisan {
-
-
+    @Inject
     private PatronInterface patronInterface;
-
     public String getData(String data){
         return patronInterface.surum(data);
     }
